db8c73f863089b257db2e9b4642fa56fc490c380
Phillip Karamagi <pkaramagi1@gmail.com>
2021-07-02 23:25:00 +0300

    Created Plugin bootstrap file, added functions for activation and deactivation

---
+++ pk-pdfgenerator.php
@@ -0,0 +1,35 @@
+<?php 
+/*
+
+*/
+
+/**
+ * Current Plugin Version
+ * version: 1.0.0
+ */
+define('PK_PDFGENERATOR_VERSION','1.0.0');
+
+/**
+ * this function executes on plugin activation
+ */
+
+function activate_pk_pdfgenerator(){
+
+}
+
+/**
+ * this function executes on plugin deactivation
+ */
+
+ function deactivate_pk_pdfgenerator(){
+
+ }
+
+/**
+ * 
+ *  */
+
+register_activation_hook( $file:string, $function:callable );
+register_deactivation_hook( $file:string, $function:callable ) 
+
+?>
