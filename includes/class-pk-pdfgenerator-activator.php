c40cf89c8cf757dba6b5c056586ab424d77a562d
Phillip Karamagi <pkaramagi1@gmail.com>
2021-07-04 14:37:33 +0300

    completed create plugin function and documentation

--- includes/class-pk-pdfgenerator-activator.php
+++ includes/class-pk-pdfgenerator-activator.php
@@ -1,4 +1,23 @@
-<?php 
+<?php
+/**
+ * Fired during plugin activation
+ *
+ * @since      1.0.0
+ *
+ * @package    Pk_Pdfgenerator
+ * @subpackage Pk_Pdfgenerator/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code needed to run during  plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Pk_Pdfgenerator
+ * @subpackage Pk_Pdfgenerator/includes
+ * @author     Phillip Karamagi <pkaramagi1@gmail.com>
+ */
 
 class Pk_Pdfgenerator_Activator{
 
@@ -6,6 +25,7 @@ class Pk_Pdfgenerator_Activator{
      * function runs on plugin activation
      */
     public static function activate(){
+
         //check if current uer can create plugins
         if(!current_user_can('activate_plugins')){
             return;
@@ -15,31 +35,43 @@ class Pk_Pdfgenerator_Activator{
             self::create_plugin_page();
         }
 
-
     }
 
-    /*
+
+	/**
+     * create plugin page on plugin activation
      *
-     * */
+     * @since    1.0.0
+     *
+     */
+
     public static function create_plugin_page(){
 
-        $page_title = null;
-        $page_content = null;
-        $page_check = get_page_by_title($page_title);
-        $current_user = wp_get_current_user();
-        $page = array(
+        $page_title = PK_PDFGENERATOR_PLUGIN_NAME;
 
+        //set page content as plugin shortcode
+        $page_content = '[pk_pdfgen]';
+        //get current user, as page author
+
+        $current_user = wp_get_current_user();
+
+        //prepare plugin page args
+        $page = array(
             'post_type' => 'page',
             'post_title' => $page_title,
             'post_content' => $page_content,
-            'post_author' => $current_user
+            'post_status' => 'publish',
+            'post_author' => $current_user->ID
         );
 
-        if(!isset($page_check->ID)){
             //create plugin page
-            wp_insert_post($page);
-        }
+            $page_id = wp_insert_post($page);
+
+            //keep track of plugin page id
+            update_option( 'pk-pdf-gen-page', $page_id );
 
     }
+
+
 
 }
